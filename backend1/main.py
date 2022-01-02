09de36cc79eadb4a3bfc7ec6787c91f7b0687061
Denys Denysko <denysko.ds@gmail.com>
2022-01-03 01:26:48 +0200

    add docker files

---
+++ backend1/main.py
@@ -0,0 +1,10 @@
+import socket
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    #return "Hello from $hostname"
+    return "Hello from {}" .format(socket.gethostname())
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5001)
